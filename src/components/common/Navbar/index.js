8b55d521f6fc0f17514e175bad5040c794f1d11d
Noman Monis <Noman.Monis@teo-intl.com>
2024-02-15 02:19:53 +0500

    fix: added animation to the navbar

--- src/components/common/Navbar/index.js
+++ src/components/common/Navbar/index.js
@@ -3,24 +3,27 @@ import './index.css'
 import Button from '../Button'
 import titleLogo from '../../../assets/titleLogo.png'
 
-
 const tabs = ['Services', 'Portfolio', 'About us']
 
 function Navbar() {
-
-  return (
-    <div className='navbar'>
-        <h1 className='nav_title'>
-     Web{' '}<img src={titleLogo} alt="Title logo" />lympus
-        </h1>
-        <div className='tab_container'>
-           {tabs?.map((tab, index) => (
-            <div key={index} className='tab'>{tab}</div>
-           ))}
-        </div>
-        <Button forwardIcon>Contact us</Button>
-    </div>
-  )
+	return (
+		<div className="navbar">
+			<div>
+				<h1 className="nav_title">
+					Web <img src={titleLogo} alt="Title logo" />
+					lympus
+				</h1>
+			</div>
+			<div className="tab_container">
+				{tabs?.map((tab, index) => (
+					<div key={index} className="tab">
+						{tab}
+					</div>
+				))}
+			</div>
+			<Button forwardIcon>Contact us</Button>
+		</div>
+	)
 }
 
 export default Navbar
--- src/components/features/Homepage/index.js
+++ src/components/features/Homepage/index.js
@@ -1,24 +1,34 @@
 import React from 'react'
-import './index.css';
-import Button from '../../common/Button';
+import './index.css'
+import Button from '../../common/Button'
 import titleLogo from '../../../assets/mainHomePageText.png'
 import scrollingIcon from '../../../assets/scrollingArrow.png'
 
-
 function Homepage() {
-  return (
-    <div className='homepage'>
-      <div className='homepage_wrapper'>
-       <img src={titleLogo} alt="Main title" />
-       <p>Elevate your online presence with cutting-edge design, seamless development, and strategic SEO. Unleash the full potential of your digital journey</p>
-       <Button className="button">Get a Quote</Button>
-       <div className='scrolling_container'>
-        <h1>SCROLL</h1>
-        <img src={scrollingIcon} alt='Scrolling arrows' />
-       </div>
-      </div>
-    </div>
-  )
+	const handleScroll = () => {
+		// Scroll to the next page, which is another 100vh down
+		window.scrollTo({
+			top: window.innerHeight, // Scroll down by the height of the window
+			behavior: 'smooth', // Smooth scrolling animation
+		})
+	}
+
+	return (
+		<div className="homepage">
+			<div className="homepage_wrapper">
+				<img src={titleLogo} alt="Main title" />
+				<p>
+					Elevate your online presence with cutting-edge design, seamless development, and strategic SEO. Unleash the
+					full potential of your digital journey
+				</p>
+				<Button className="button">Get a Quote</Button>
+				<div className="scrolling_container" onClick={handleScroll}>
+					<h1>SCROLL</h1>
+					<img src={scrollingIcon} alt="Scrolling arrows" />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Homepage
